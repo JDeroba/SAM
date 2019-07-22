626da2123b280b0897abdc996e0a30a41f44c814
jonathanderoba <jonathan.deroba@noaa.gov>
2019-07-22 16:23:41 -0400

    Work on loop code

--- SAM_run_looped.R
+++ SAM_run_looped.R
@@ -3,14 +3,19 @@ library(stockassessment)
 source(paste("C:\\Users\\jonathan.deroba\\Documents\\GitHub\\SAM-ICES-WG-SR-one-off","functions.R",sep="\\")) #shouldn't have to touch
 #library(TMB)
 
-direct<-"C:\\Users\\jonathan.deroba\\Documents\\GitHub\\wg_MGWG-master\\stock-recruitment\\comparing_recruitment\\simulated_stocks\\sa"
+direct<-"B:\\jderoba\\ICES_AMWG_SR\\sa\\SAM_BH"
 
 files<-list.files(direct) 
 
+
+
 for(r in 1:1){ #loop over OM level folders
   r.folder<-paste(direct,files[r],sep="\\") #OM level (r) folder
-  iters<-list.files(r.folder) #datasets within OM folder
-  for(i in 1:1){  #length(iters)){  #loop over datasets within OM
+  iters<-list.files(r.folder,pattern="iter") #datasets within OM folder
+  converge.count<-0 #count the number of converged runs
+  SRparms<-c() #matrix(NA,2,8)  #container for all Stock recruit param estimates
+  for(i in 1:2){  #length(iters)){  #loop over datasets within OM
+    
     iter<-paste(r.folder,iters[i],sep="\\") #single dataset folder
     #read in single dataset
     cn <- read.ices(paste(iter,"sim-LANUM.txt",sep="\\"))
@@ -44,14 +49,28 @@ for(r in 1:1){ #loop over OM level folders
     par<-defpar(dat,conf) #some default starting values
     #turn off survival process variance and set to 0
     par$logSdLogN[2]<-1 #set sd of logN age2-20 to 0 (1 in log space)
-    fit<-sam.fit(dat,conf,par,run=T,map=list("logSdLogN"=factor(c(1,NA)))) #fit the model
+    fit<-try(sam.fit(dat,conf,par,run=T,map=list("logSdLogN"=factor(c(1,NA))))) #fit the model
     
+    if(is.null(attr(fit,"condition"))){
+    saveRDS(fit,file=paste(iter,"SAMfit.RData",sep="\\" )) #save the results
+    converge.count<-converge.count+1
     modelTable<-modeltable(fit) #AIC and number of params
     write.csv(modelTable,file=paste(iter,"ModelTable.csv",sep="\\"))
+    if(fit$conf$stockRecruitmentModelCode==2){
+    alpha<-partable(fit)["rec_loga_0","exp(par)"]
+    beta<-partable(fit)["rec_logb_0","exp(par)"]
+    alpha.flr<-alpha/beta
+    beta.flr<-1/beta
+    
+    natMor<- fit$data$natMor[nrow(fit$data$natMor),]
+    StWt<-fit$data$stockMeanWeight[nrow(fit$data$stockMeanWeight),]
+    Mature<-fit$data$propMat[nrow(fit$data$propMat),]
+    SRparmsa<-SR.parms(nage=max(fit$data$maxAgePerFleet),M=natMor,Wt=StWt,Mat=Mature,alpha=alpha,beta=beta,type=fit$conf$stockRecruitmentModelCode)
+    SRparms<-rbind(SRparms,cbind(SRparmsa,alpha.flr,beta.flr,files[r],iters[i]) )
+    } 
     
     ##Will save fit details and diagnostic plots pdf if TRUE
-    if(TRUE){
-      saveRDS(fit,file=paste(iter,"SAMfit.RData",sep="\\" )) #save the results
+    if(FALSE){
       qplotnames<-getnames(confa=conf$keyLogFpar[2:nrow(conf$keyLogFpar),],agesa=ages,looptoa=(max(conf$keyLogFpar)+1),looptob=(length(names(surveys))),namesa=names(surveys))
       Fpronames<-getnames(confa=t(as.matrix(conf$keyVarF[1,])),agesa=ages,looptoa=(max(conf$keyVarF)+1),looptob=1,namesa="SD LogF process")
       Npronames<-getnames(confa=t(as.matrix(conf$keyVarLogN)),agesa=ages,looptoa=(max(conf$keyVarLogN)+1),looptob=1,namesa="SD LogN process")
@@ -62,5 +81,8 @@ for(r in 1:1){ #loop over OM level folders
       #make the plots and save to pdf in Run sub-directory
       plotfxn(afit=fit,datdirect=r.folder,run=iters[i],confa=conf,qplotnamesa=qplotnames,varplotnamesa=varplotnames,fstanamesa=Fstanames,catchmultnamesa=catchmultnames,retroyrs=2) #as.vector(seq(2011,2014)))
     } #end if for diagnostic plots  
+    } #close if/try
   } #i loop
+  write.csv(SRparms,paste(direct,paste(files[r],"SRparms.csv",sep="\\"),sep="\\"))
+  write.csv(converge.count,paste(direct,paste(files[r],"converge_count.csv",sep="\\"),sep="\\"))
 } # r loop
